17bfb24cd7d2c14436c0aff3c4aab598f22e3e56
ojhalakshya <ojhalakshya@gmail.com>
2020-12-01 11:21:51 +0530

    changes

--- studious-eureka/src/App.js
+++ studious-eureka/src/App.js
@@ -1,19 +1,21 @@
-import Home from './Components/Home'
-import Video from './Components/Video'
-import './App.css';
+import React, { Component } from 'react'
+import Video from './Video'
+import Home from './Home'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/:url" component={Video} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Router>
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/:url" component={Video} />
+					</Switch>
+				</Router>
+			</div>
+		)
+	}
 }
 
 export default App;
